73755b420b2fa0048fd44214923a6f25d76e67b9
Rebeca Neves <rebeca.rochaneves@gmail.com>
2026-09-23 12:56:00 -0300

    Subindo URL gerada pelo render (deploy)

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -2,4 +2,4 @@ export const API_BASE_URL =
   window.location.hostname === "localhost" ||
   window.location.hostname === "127.0.0.1"
     ? "http://localhost:3000"
-    : "https://SEU-BACKEND.onrender.com";
+    : "https://beca-list.onrender.com/";
