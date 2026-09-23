a173731d2e81f9368d0c71ad6be5d88967a06bc6
Rebeca Neves <rebeca.rochaneves@gmail.com>
2026-09-23 13:23:28 -0300

    Correção de bug

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -2,4 +2,4 @@ export const API_BASE_URL =
   window.location.hostname === "localhost" ||
   window.location.hostname === "127.0.0.1"
     ? "http://localhost:3000"
-    : "https://beca-list.onrender.com/";
+    : "https://beca-list.onrender.com";
